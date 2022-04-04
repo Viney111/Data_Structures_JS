8a93c4fd6cf5428af92dc252dbf891cef23ab2f4
Viney <vineykhaneja999@gmail.com>
2022-04-04 23:03:34 +0530

    [Viney] ADDED: Finding repeated numbers between 0 to 100

---
+++ FindDigitsRepeatedTwice.js
@@ -0,0 +1,11 @@
+//Find numbers like 33,77 etc between 0-100.
+function isRepeated(number) {
+    if (number % 11 == 0) return true;
+    else return false;
+}
+  
+let repeatedNumbersArray = new Array();
+for (let i = 1; i < 100; i++) {
+    if (isRepeated(i)) repeatedNumbersArray.push(i);
+}
+  console.log(repeatedNumbersArray);
